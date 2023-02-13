ae250e6e7a4faab08d5ec06e7c3b8ea37f8d2170
GrigoriyBabikhin <Grigoriy.Babikhin@gmail.com>
2023-02-13 10:37:14 +0500

    Task

---
+++ Comparison operators.js
@@ -0,0 +1,8 @@
+let a1=5 > 4//true
+let a2="ананас" > "яблоко"//false
+let a3="2" > "12"//true
+let a4=undefined == null//true
+let a5=undefined === null//false
+let a6=null == "\n0\n"//false
+let a7=null === +"\n0\n"//falses
+console.log(a7)
