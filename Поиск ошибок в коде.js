6fc91c395a853bad636bcec9a295f918d3dd24f1
GrigoriyBabikhin <Grigoriy.Babikhin@gmail.com>
2023-02-27 10:53:35 +0500

    Finding errors in the code.

---
+++ Поиск ошибок в коде.js
@@ -0,0 +1,51 @@
+//Код должен находить сумму чисел:
+let num1 = 10;
+let num2 = 5;
+console.log(num1 + num2);
+//Код должен находить сумму чисел:
+let a = 1;
+let b = 2;
+console.log(a + b);
+//Код должен находить сумму цифр числа:
+let num = "1234";
+let sum =
+  parseInt(num[0]) + parseInt(num[1]) + parseInt(num[2]) + parseInt(num[3]);
+console.log(sum);
+//Код должен вывести первую цифру числа:
+let num3 = String(123);
+console.log(num3[0]);
+//Код должен был вывести в консоль число 1, но выводит 0:
+let num4 = 0;
+console.log(++num4);
+//Код должен вывести количество цифр в числе:
+let num5 = String(123);
+console.log(num5.length);
+//Код должен найти количество секунд в сутках:
+let num6 = 24 * 60 * 60;
+console.log(num6);
+//Код должен вывести количество цифр в числе:
+let num7 = 12345;
+let str7 = String(num7);
+console.log(str7.length);
+//Код должен вывести последнюю цифру числа:
+let num8 = 12345678;
+let str8 = String(num8);
+console.log(str8[str8.length - 1]);
+//Код должен вывести количество цифр в числе:
+let num9 = 123456789;
+let str9 = String(num9);
+console.log(str9.length);
+//Код должен вывести последнюю цифру числа:
+let num10 = 1234567;
+let str10 = String(num10);
+console.log(str10[str10.length - 1]);
+//Код должен найти сумму чисел:
+let a1 = "123";
+let b1 = "456";
+let s1 = Number(a1) + Number(b1);
+console.log(s1);
+//Код должен найти сумму чисел:
+let aaa = 1;
+let bbb = 2;
+let ccc = 3;
+console.log(aaa + bbb + ccc);
